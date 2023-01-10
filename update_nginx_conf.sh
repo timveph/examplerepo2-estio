c8bb77d4abdfe7b328ccd7ad57f7e1db9c0ea652
timveph <timveph@users.noreply.github.com>
2023-01-10 15:23:48 +0000

    dynamically pass server address

--- update_nginx_conf.sh
+++ update_nginx_conf.sh
@@ -1,9 +1,11 @@
+public_ip=$(curl http://checkip.amazonaws.com)
+
 cat <<EOF > /etc/nginx/nginx.conf
 events{}
 http {
     server {
       listen 80;
-      server_name 3.8.119.98/;
+      server_name $public_ip;
       location / {
           proxy_pass http://3.8.119.98/:5000;
       }
